6177cc693848c270d578d2058c04a3695e89d687
dustContributor <hctf90@gmail.com>
2022-08-14 00:05:04 -0300

    - Forgot to update the data type api.

--- historizr-server/src/io/historizr/server/api/DataTypeApi.java
+++ historizr-server/src/io/historizr/server/api/DataTypeApi.java
@@ -2,13 +2,16 @@ package io.historizr.server.api;
 
 import static io.historizr.server.OpsReq.failed;
 
-import io.historizr.server.OpsMisc;
+import java.util.stream.Collectors;
+
+import io.historizr.server.db.DataType;
 import io.historizr.server.db.Db;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.ext.web.Router;
+import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.SqlClient;
 
-public class DataTypeApi {
+public final class DataTypeApi {
 	private DataTypeApi() {
 		throw new RuntimeException();
 	}
@@ -16,17 +19,18 @@ public class DataTypeApi {
 	private static final String ROUTE = "/datatype";
 
 	public static Router register(EventBus bus, Router router, SqlClient conn) {
+		var toModels = Collectors.mapping((Row r) -> DataType.of(r), Collectors.toList());
+		var modelType = DataType.class;
 		router.get(ROUTE).handler(ctx -> {
-			conn.query(Db.Sql.QUERY_DATA_TYPE).execute(r -> {
-				if (failed(r, ctx)) {
-					return;
-				}
-
-				var res = OpsMisc.stream(r)
-						.map(io.historizr.server.db.DataType::of)
-						.toList();
-				ctx.json(res);
-			});
+			conn.query(Db.Sql.QUERY_DATA_TYPE)
+					.collecting(toModels)
+					.execute(r -> {
+						if (failed(r, ctx)) {
+							return;
+						}
+						var res = r.result().value();
+						ctx.json(res);
+					});
 		});
 		return router;
 	}
